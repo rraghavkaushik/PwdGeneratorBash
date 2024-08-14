24ef73e62b3a529b4fd6d6d45e5a3b330fccf2db
rraghavkaushik <136466980+rraghavkaushik@users.noreply.github.com>
2024-08-14 22:22:57 +0530

    Update pwd_generator.sh

--- src/pwd_generator.sh
+++ src/pwd_generator.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+display() {
+    echo "Usage: $0 [-l] [-u] [-s] [-n] [-v]"
+    # echo "Generating a random pwd..."
+    echo "  -l LENGTH  Specify the password length (default: 10)"
+    echo "  -u          Include uppercase letter"
+    echo "  -s          Include special characters"
+    echo "  -n          Include number"
+    echo "  -v          verbose mode"
+
+    exit 1
+
+}
+
+verbose() {
+    if [["$VERBOSE" == "true"]]; then
+        echo "$@"
+    fi
+}
+
+LENGTH = 10
+UPPERCASE = false
+SPECIAL = false
+NUMBERS = false
+VERBOSE = false
+
+
+
 
